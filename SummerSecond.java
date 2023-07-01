f9c73119298823cbd8a8eb3bc8a5ea23a468e27d
CnBatuhan <138293421+CnBatuhan@users.noreply.github.com>
2023-07-01 13:02:15 +0300

    Add files via upload

---
+++ SummerSecond.java
@@ -0,0 +1,7 @@
+public class SummerSecond {
+    public static void main(String[] args){
+        System.out.println(2+3);   // Eğer tırnak içerisinde değilse toplar.
+        System.out.println("2+3"); // Tırnak içerisinde olursa toplama işlemi yapmaz.
+        System.out.println(7*7); // Çarpım işlemi olur.
+    }
+}
---
+++ Summerfirst.java
@@ -0,0 +1,5 @@
+public class Summerfirst {
+    public static void main(String[] args){
+        System.out.println("HELLO BATUHAN");
+    }
+}
