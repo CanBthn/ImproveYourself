badd7303636f552d1c237baac5bd1db3d1b9c068
CnBatuhan <138293421+CnBatuhan@users.noreply.github.com>
2023-07-01 21:03:13 +0300

    Delete Input.java

--- Input.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-public class Input {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("PLEASE ENTER VALİD VALUE");
-        int a = sc.nextInt();
-        System.out.println("YOUR VALUE İS = " + a);
-    }
-}
