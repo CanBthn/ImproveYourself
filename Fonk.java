331199efd28a560f054188c301896ec4b5ed7354
CnBatuhan <138293421+CnBatuhan@users.noreply.github.com>
2023-07-01 21:03:02 +0300

    Delete Fonk.java

--- Fonk.java
+++
@@ -1,8 +0,0 @@
-public class Fonk {
-    public static void main (String[] args){
-        //Bir üçgen düşünelim bir kenarı 3 diğer kenarı 4 bu üçgende 3-4-5 üçgeni olsun 5 i nasıl buluruz.
-        int a = 3; //Kısa kenar.
-        int b = 4; //Dikey kenar.
-        System.out.println((a*a)+(b*b) + " Karekökü alındığında hipotenüs değeri çıkacakatır.");
-    }
-}
